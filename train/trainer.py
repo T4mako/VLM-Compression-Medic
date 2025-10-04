4a7714283260e13aae6166c6eaa95edb38d3ebb0
T4mako <1536954632@qq.com>
2025-10-04 22:13:19 +0800

    fix config bug

--- train/trainer.py
+++ train/trainer.py
@@ -52,12 +52,12 @@ class OBRTrainer:
 
     def compress_model(self):
         model, processor = model, processor = load_huatuo_vision_model(
-            config.model.model_name,
-            device=config.training.device,
-            vit_compress_mode=config.model.vit_compress_mode,
-            vit_target_tokens=config.model.vit_target_tokens
+            Config.model.model_name,
+            device=Config.training.device,
+            vit_compress_mode=Config.model.vit_compress_mode,
+            vit_target_tokens=Config.model.vit_target_tokens
         )
-        calib_data = load_pubmed_vision(self.config)
+        calib_data = load_pubmed_vision(Config)
         activations = self.calibrate_activations(model, processor, calib_data)
 
         # Compress language model
